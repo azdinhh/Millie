78e3a917d3fd96254b9cf67d4b004f4f09694f55
AYANUKOJI-KEN <100865866+Yahya910@users.noreply.github.com>
2022-03-20 13:35:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -32,15 +32,15 @@ global.APIKeys = { // The APIKey is here
 }
 
 // Sticker WM
-global.packname = '💋 ᴍɪʟʟɪᴇ 💋'
-global.author = 'Ⓝ🅔Ⓔ🅡Ⓐ🅙'
+global.packname = '💋 بووووووت سيييي 💋'
+global.author = 'YANOKUJI'
 
 
 global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://github.com/neera-j/Bot-Files/blob/main/images/millie.jpg'
 
-global.watermark = '© ᴍɪʟʟɪᴇ'
+global.watermark = '© يحيى'
 
 global.multiplier = 69 // The higher it is, the harder it is to level up
 
